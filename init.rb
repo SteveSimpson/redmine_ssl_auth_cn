d2064461adea2c30582f83a5b5084853ab514cd1
Steve Simpson <steve.simpson.ctr@dren.hpc.mil>
2015-05-28 11:47:58 -0400

    Updated installation steps in doc.

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 require 'ext/account_controller'
 
 Redmine::Plugin.register :redmine_ssl_auth do
-  name 'Redmine SSL auth plugin where Username is CNAME'
+  name 'Redmine SSL auth plugin where Username is the common name in the certificate.'
   author 'Steve Simpson'
   description 'Enable authentication using SSL client certificates'
   version '0.1.1'
