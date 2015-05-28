00dff0854318874f1b69a57993653d7084e6ab23
Steve Simpson <steve.simpson.ctr@dren.hpc.mil>
2015-05-28 12:16:50 -0400

    Updated documentation to reflect new name.

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 require 'redmine'
 require 'ext/account_controller'
 
-Redmine::Plugin.register :redmine_ssl_auth do
-  name 'Redmine SSL auth plugin where Username is the common name in the certificate.'
+Redmine::Plugin.register :redmine_ssl_auth_cn do
+  name 'Redmine SSL Auth CN plugin'
   author 'Steve Simpson'
-  description 'Enable authentication using SSL client certificates'
+  description 'Enable authentication using SSL client certificates where username=[certificate common name].'
   version '0.1.1'
 end
